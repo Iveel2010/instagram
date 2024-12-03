c179cfa9209f1eaac0eab8eea459a522f9aac9d5
24HP0818 24HP0818 <24HP0818@admins-iMac.local>
2024-12-03 18:11:19 +0800

    hi

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 dotenv.config();
 const port = 8080;
 const useRouter = require("./Route/userRouter");
